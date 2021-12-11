9fdf91e107e6211751cba4b80f397c3da81123ff
jasontjz <85021183+jasontjz@users.noreply.github.com>
2021-12-11 09:45:54 +0800

    fix gitignore

--- frontend/src/components/cart/Cart.js
+++ frontend/src/components/cart/Cart.js
@@ -9,6 +9,18 @@ const Cart = () => {
 
   const { cartItems } = useSelector((state) => state.cart);
 
+  const increaseQty = (id, quantity, stock) => {
+    const newQty = quantity + 1;
+    if (newQty > stock) return;
+    dispatch(addItemToCart(id, newQty));
+  };
+
+  const decreaseQty = (id, quantity) => {
+    const newQty = quantity - 1;
+    if (newQty <= 0) return;
+    dispatch(addItemToCart(id, newQty));
+  };
+
   return (
     <Fragment>
       {cartItems.length === 0 ? (
@@ -46,15 +58,33 @@ const Cart = () => {
 
                       <div className="col-4 col-lg-3 mt-4 mt-lg-0">
                         <div className="stockCounter d-inline">
-                          <span className="btn btn-danger minus">-</span>
+                          <span
+                            className="btn btn-danger minus"
+                            onClick={() =>
+                              decreaseQty(item.product, item.quantity)
+                            }
+                          >
+                            -
+                          </span>
                           <input
                             type="number"
                             className="form-control count d-inline"
-                            value="1"
+                            value={item.quantity}
                             readOnly
                           />
 
-                          <span className="btn btn-primary plus">+</span>
+                          <span
+                            className="btn btn-primary plus"
+                            onClick={() =>
+                              increaseQty(
+                                item.product,
+                                item.quantity,
+                                item.stock
+                              )
+                            }
+                          >
+                            +
+                          </span>
                         </div>
                       </div>
 
