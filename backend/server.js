79e3071a2befa659ac62ac11b1de17ffc111c84b
jasontjz <85021183+jasontjz@users.noreply.github.com>
2021-12-12 19:43:41 +0800

    get rid of the production or development mode

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,8 @@ process.on("uncaughtException", (err) => {
 });
 
 // setting up config file
-if (process.env.NODE_ENV !== "PRODUCTION")
-  require("dotenv").config({ path: "backend/config/config.env" });
+// if (process.env.NODE_ENV !== "PRODUCTION")
+require("dotenv").config({ path: "backend/config/config.env" });
 
 // Connecting to database
 connectDatabase();
